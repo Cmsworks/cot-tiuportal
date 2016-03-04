e991b093f8a708fa28ffd1bb4bf61ba06ef932f4
devkont <yusupovb@gmail.com>
2016-03-04 21:37:16 +0300

    fixed #28

--- modules/payments/payments.setup.php
+++ modules/payments/payments.setup.php
@@ -4,7 +4,7 @@
  * Code=payments
  * Name=Payments
  * Description=Платежный модуль
- * Version=1.1.8.1
+ * Version=2.0
  * Date=
  * Author=CMSWorks Team
  * Copyright=Copyright (c) CMSWorks.ru, littledev.ru
@@ -36,7 +36,7 @@
  * Payments module
  *
  * @package payments
- * @version 1.1.8.1
+ * @version 2.0
  * @author CMSWorks Team
  * @copyright Copyright (c) CMSWorks.ru
  * @license BSD
